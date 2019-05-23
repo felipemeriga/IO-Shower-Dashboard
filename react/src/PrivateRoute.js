93842cd40b37ac9a2aff87fa549e64299723128a
Meriga EC2 <felipe.meriga@gmail.com>
2019-05-23 20:31:07 +0000

    Modifying the route component

--- react/src/PrivateRoute.js
+++ react/src/PrivateRoute.js
@@ -2,6 +2,7 @@ import React from "react";
 import {inject, observer} from "mobx-react";
 import {Redirect, Route} from "react-router-dom";
 import {TOKEN_KEY} from "./stores/SessionStore";
+import getAuthToken from "./stores/SessionStore";
 
 // This way of doing was changed because of the async method of getting the token
 /*
@@ -30,13 +31,53 @@ class PrivateRoute extends React.Component {
     }
   }
 
-  componentWillMount() {
+  isAuthenticated = () => {
+    let currentState = {
+      isAuthenticated: false,
+      loading: true
+    };
     localStorage.removeItem(TOKEN_KEY);
     console.log(localStorage.getItem(TOKEN_KEY));
+    if (localStorage.getItem(TOKEN_KEY) !== null) {
+      console.log("aee");
+      currentState.isAuthenticated = true;
+      currentState.loading = false;
+      this.changeState(currentState);
+    } else {
+      getAuthToken().then((token) => {
+        debugger;
+        console.log("Entrou aqui!");
+        debugger;
+        console.log(token);
+        if (token.data === "") {
+          console.log("return false");
+          currentState.isAuthenticated = false;
+        } else {
+          console.log("return true");
+          localStorage.setItem(TOKEN_KEY, token);
+          currentState.isAuthenticated = true;
+        }
+        currentState.loading = false;
+        this.changeState(currentState);
+      }).catch((err) => {
+        console.log(err);
+        currentState.isAuthenticated = false;
+        currentState.loading = false;
+        this.changeState(currentState);
+      })
+    }
+  };
 
-
-
+  componentWillMount() {
+    this.isAuthenticated();
   }
+
+  changeState = (result) => {
+    this.setState({
+      loading: result.loading,
+      isAuthenticated: result.isAuthenticated
+    })
+  };
 
   render() {
     const {component: Component, ...rest} = this.props;
@@ -45,10 +86,10 @@ class PrivateRoute extends React.Component {
       <Route
         {...rest}
         render={props =>
-          this.props.sessionStore.authenticated ? (
+          this.state.isAuthenticated ? (
             <Component {...props} />
           ) : (
-            this.props.sessionStore.loading ? (
+            this.state.loading ? (
               <div className="animated fadeIn pt-1 text-center">Loading...</div>
             ) : (
               <Redirect to={{pathname: '/login', state: {from: this.props.location}}}/>
--- react/src/stores/SessionStore.js
+++ react/src/stores/SessionStore.js
@@ -1,51 +1,12 @@
 // @flow
-import {observable, action, decorate} from "mobx";
 import api from "../services/api";
 
 export const TOKEN_KEY = "@shower-token";
 
-class SessionStore {
 
-  authenticated: boolean = false;
-  loading: boolean = true;
 
-  isAuthenticated = () => {
-    this.authenticated = false;
-    this.loading = true;
-    localStorage.removeItem(TOKEN_KEY);
-    console.log(localStorage.getItem(TOKEN_KEY));
-    if (localStorage.getItem(TOKEN_KEY) !== null) {
-      console.log("aee");
-      this.authenticated = true;
-    } else {
-      this.getAuthToken().then((token) => {
-        if (token.data === undefined) {
-          console.log("return false");
-          this.authenticated = false;
-        } else {
-          console.log("return true");
-          localStorage.setItem(TOKEN_KEY, token);
-          this.authenticated = true;
-        }
-        this.loading = false;
-      }).catch((err) => {
-        console.log(err);
-        this.authenticated = false;
-        this.loading = false;
-      })
-    }
-  };
-
-  getAuthToken(): any {
-    return api.get("/auth/testToken");
-  }
+function getAuthToken(): any {
+  return api.get("/auth/testToken");
 }
 
-decorate(SessionStore, {
-  authenticated: observable,
-  isAuthenticated: action,
-  getAuthToken: action,
-  loading: observable
-});
-
-export default new SessionStore();
+export default getAuthToken;
